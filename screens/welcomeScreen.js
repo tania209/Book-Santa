7e176b2d79e8a921dd61243f75de2904fdefd63e
tania209 <70532882+tania209@users.noreply.github.com>
2021-05-15 11:43:49 -0700

    Add files via upload

---
+++ screens/welcomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class WelcomeScreen extends React.Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Book Santa
+                </Text>
+             </View>
+        )
+
+    }
+}
